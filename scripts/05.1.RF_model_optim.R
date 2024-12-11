609d90fd9973c0395131e0564641caf72371034c
rhijmans <r.hijmans@gmail.com>
2024-12-11 12:32:48 -0800

    fix seeds

--- scripts/05.1.RF_model_optim.R
+++ scripts/05.1.RF_model_optim.R
@@ -3,10 +3,9 @@
 
 rf_optim <- function(x) {
 
-	# input and ouptut foders and files
+# input and ouptut foders and files
 	input_path <- "data"
 	output_path <- "output"
-
 
 	treatment <- paste0(x[,1:3], collapse="-")
 	outfile <- paste0("RFoptim_", treatment, "_mbucket-", x$mbuck, ".Rds") 
@@ -17,15 +16,13 @@ rf_optim <- function(x) {
 
 	lsms_spatial <- readRDS(file.path(input_path, "lsms_trimmed_95th_africa.Rds"))
 
-	# keep only variables needed in the model
+# keep only variables needed in the model
 	lsms_spatial <- lsms_spatial |>  dplyr::select(farm_area_ha, cropland, cattle, pop, cropland_per_capita,
 			 sand, slope, temperature, rainfall, maizeyield, market) |> na.omit() 
 
-	# ------------------------------------------------------------------------------
-	# set parms
-	seeds_list[[11]] <- sample.int(10000, size = 1) 
-	ctrl <- caret::trainControl(method = 'cv', number = 10, savePredictions = 'all', seeds = seeds_list)
-
+# ------------------------------------------------------------------------------
+	seeds_list <- as.list(c(x[,paste0("seed_", 1:10)], sample(10000, 1)))
+	ctrl <- caret::trainControl(method = "cv", number = 10, savePredictions = "all", seeds = seeds_list)
 
 	rf_full_model <- caret::train(
 		farm_area_ha ~ .,
@@ -50,12 +47,17 @@ rf_optim <- function(x) {
 tune_grid <- expand.grid(
 	mtry = 3:10,										 
 	splitrule = c("variance", "extratrees"),											 
-	min.node.size = c(5, 40:60, 100, 200),
-	mbuck = c(1, 5, seq(10, 100, 10), 200)
+	min.node.size = c(5, 40:60, 100, 200)
 )
 # for the output filename
 tune_grid$splitrule <- as.character(tune_grid$splitrule)
 
+seeds <- sapply(1:10, \(i) sample.int(10000, size = nrow(tune_grid)))
+colnames(seeds) <- paste0("seed_", 1:10)
+tune_grid <- cbind(tune_grid, seeds)
+
+mbuck <- c(1, 5, seq(10, 100, 10), 200)
+tune_grid <- data.frame(tune_grid[rep(1:nrow(tune_grid), each=length(mbuck)), ], mbuck=mbuck)
 
 # sequential
 # for (i in 1:nrow(tune_grid)) (rf_optim(tune_grid[i,]))
@@ -70,4 +72,4 @@ if (i <= nrow(tune_grid)) {
 }
 
 # slurm options
-#sbatch --array=1-4992 -p bmh --time=600 --mem=16G --job-name=farms ~/farm/clusterR.sh scripts/05.1.RF_model_optim.R
+#sbatch --array=1-4992 -p bmh --time=1200 --mem=32G --job-name=farms ~/farm/clusterR.sh scripts/05.1.RF_model_optim.R
