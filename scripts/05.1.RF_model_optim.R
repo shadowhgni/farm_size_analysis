55e9bf622d8e5e66e288f290241ad0fd6063dc5e
rhijmans <r.hijmans@gmail.com>
2024-12-11 11:04:16 -0800

    parallelization

--- scripts/05.1.RF_model_optim.R
+++ scripts/05.1.RF_model_optim.R
@@ -1,89 +1,72 @@
 # Random forest optimization
-
-
-#-------------------------------------------------------------------------------
-# load packages
-require(tidyverse) # sorry dear Robert, I felt lazy to recode in basic R
-require(caret)
-require(ranger)
-
-# Clean environment
-rm(list=ls())
-
-# Set working directory
-#setwd(here::here())
 # ------------------------------------------------------------------------------
-# create directories if needed
-if(!dir.exists('../data'))
-  dir.create('../data') # drop the input files here
-if(!dir.exists('../output'))
-  dir.create('../output') 
 
-# define input and ouptut foders and files # please modify accordingly!
-input_path <- '../data'
-output_path <- '../output'
-model_result_file2 <- '../output/model_optimization.Rdata'
+rf_optim <- function(x) {
 
-# ------------------------------------------------------------------------------
-# Prepare data
-load('../data/lsms_trimmed_95th_africa.rdata') 
-stacked <- terra::rast('../data/stacked_rasters_africa.tif')
+	# input and ouptut foders and files
+	input_path <- "data"
+	output_path <- "output"
 
-# ------------------------------------------------------------------------------
-# data wrangling
 
-# keep only variables needed in the model
-df <- lsms_spatial |>
-  select(farm_area_ha, cropland, cattle, pop, cropland_per_capita,
-         sand, slope, temperature, rainfall, maizeyield, market) |>
-  na.omit() 
+	treatment <- paste0(x[,1:3], collapse="-")
+	outfile <- paste0("RFoptim_", treatment, "_mbucket-", x$mbuck, ".Rds") 
+	print(paste("-------- run =", outfile, "----------"))
+	dir.create(output_path, FALSE, FALSE) 
+	outfile <- file.path(output_path, outfile)
+	# if (file.exists(output_file)) return("file existed")
 
-# ------------------------------------------------------------------------------
-# set parms
+	lsms_spatial <- readRDS(file.path(input_path, "lsms_trimmed_95th_africa.Rds"))
 
+	# keep only variables needed in the model
+	lsms_spatial <- lsms_spatial |>  dplyr::select(farm_area_ha, cropland, cattle, pop, cropland_per_capita,
+			 sand, slope, temperature, rainfall, maizeyield, market) |> na.omit() 
+
+	# ------------------------------------------------------------------------------
+	# set parms
+	train_control <- caret::trainControl(method = "cv", number = 10, savePredictions = "all", seeds = 2024)
+
+
+	rf_full_model <- caret::train(
+		farm_area_ha ~ .,
+		data = lsms_spatial,
+		method = "ranger",
+		preProcess = c("center", "scale", "spatialSign"),
+ #	 trControl = train_control,
+		keep.inbag = TRUE,
+		tuneGrid = x[, 1:3],
+		importance	= "permutation",
+		metric = "RMSE",
+		min.bucket = x$mbuck,
+		num.trees = 500
+	)
+	
+	saveRDS(rf_full_model, file = outfile)
+	rf_full_model$results
+}
+
+
+# input combinations
 tune_grid <- expand.grid(
-  mtry = 3:10,                     
-  splitrule = c('variance', 'extratrees'),                       
-  min.node.size = c(5, 40:60, 100, 200)
+	mtry = 3:10,										 
+	splitrule = c("variance", "extratrees"),											 
+	min.node.size = c(5, 40:60, 100, 200),
+	mbuck = c(1, 5, seq(10, 100, 10), 200)
 )
-
-set.seed(2024) 
-seeds_list <- vector('list', length = 11) 
-for (i in 1:10) {
-  seeds_list[[i]] <- sample.int(10000, size = nrow(tune_grid))
-}
-seeds_list[[11]] <- sample.int(10000, size = 1) 
-ctrl <- caret::trainControl(method = 'cv', number = 10, savePredictions = 'all', seeds = seeds_list)
+# for the output filename
+tune_grid$splitrule <- as.character(tune_grid$splitrule)
 
 
-rf_optim <- function(ind = 1, mbuck = mbuck){
-  my_grid <- tune_grid[ind, ]
-  rf_full_model <- caret::train(
-    farm_area_ha ~ .,
-    data = df,
-    method = 'ranger',
-    preProcess = c('center', 'scale', 'spatialSign'),
-    trControl = ctrl,
-    keep.inbag = T,
-    tuneGrid = my_grid,
-    importance  = 'permutation',
-    metric = 'RMSE',
-    min.bucket = mbuck,
-    num.trees = 500
-  )
-  print(paste0('-------- run = ', ind, '_min.bucket = ', mbuck,'----------'))
-  print(rf_full_model$results)
-  saveRDS(rf_full_model, file = paste0(output_path, '/RF_optim_run_', ind, '__mbucket_', mbuck, '.RDS'))
-  return(rf_full_model$results)
+# sequential
+# for (i in 1:nrow(tune_grid)) (rf_optim(tune_grid[i,]))
+
+# parallel
+i <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+if (i <= nrow(tune_grid)) {
+	r <- rf_optim(tune_grid[i,])
+	print(r)
+} else {
+	print("done (i > nrow(tune_grid)")
 }
 
-
-# run function in a loop
-deb <- Sys.time()
-
-for(i in 1:nrow(tune_grid)){
-  for(m in c(1, 5, seq(10, 100, 10), 200)){ # min.bucket
-    rf_optim(ind = i, mbuck = m)
-  }
-}
-fin <- Sys.time() - deb; print(fin)
+# slurm options
+#sbatch --array=1-4992 -p bmh --time=600 --mem=16G --job-name=farms ~/farm/clusterR.sh scripts/05.1.RF_model_optim.R
