ffa4fc51f7d2bfbf07d54e2032c2c43446faf8d2
rhijmans <r.hijmans@gmail.com>
2025-01-26 11:59:08 -0800

    paths

--- scripts/05.1.RF_model_optim.R
+++ scripts/05.1.RF_model_optim.R
@@ -4,8 +4,11 @@
 rf_optim <- function(x) {
 
 # input and ouptut foders and files
-	input_path <- "data"
-	output_path <- "output"
+#	input_path <- "data"
+#	output_path <- "output"
+	input_path <- "data/processed"
+	output_path <- "output/RFoptim"
+	dir.create(output_path, FALSE, TRUE)
 
 	treatment <- paste0(x[,1:3], collapse="-")
 	outfile <- paste0("RFoptim_", treatment, "_mbucket-", x$mbuck, ".Rds") 
