403a59ce0aeeec693c1736932c5e5c597ccd360d
Deo-Gratias Hougni <156960649+shadowhgni@users.noreply.github.com>
2026-04-11 11:08:49 +0200

    Delete scripts directory
    Signed-off-by: Deo-Gratias Hougni <156960649+shadowhgni@users.noreply.github.com>

--- scripts/05.2.RF_model_optim_summarized.R
+++
@@ -1,8 +0,0 @@
-# Compile results of RF optimization
-
-ff <- list.files("output/RFoptim", pattern="\\.Rds$", full=TRUE)
-res <- lapply(ff, \(f) readRDS(f)$results)
-
-res <- data.frame(filename=basename(ff), do.call(rbind, res))
-res <- res[order(-res$Rsquared, res$RMSE, res$MAE), ]
-saveRDS(res, "output/tables/RF_optim_summarized_table.rds")
